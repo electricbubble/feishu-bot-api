38a5e0974a217f7de4a17a1ae34f66ab6bd4062d
electricbubble <bubble_yang@yeah.net>
2023-02-21 23:20:50 +0800

    chore: 移除已弃用代码段

--- api.go
+++ api.go
@@ -40,67 +40,8 @@ const (
 	LangJapanese Language = "ja_jp"
 )
 
-// var _ fmt.Stringer = (*apiResponse)(nil)
-
 type apiResponse struct {
 	Code int    `json:"code"`
 	Msg  string `json:"msg"`
 	Data any    `json:"data"`
 }
-
-// var HTTPClient = http.DefaultClient
-//
-// func newRequest(method string, rawUrl string, rawBody []byte) (request *http.Request, err error) {
-// 	debugLog(fmt.Sprintf("--> %s %s\n%s", method, rawUrl, rawBody))
-//
-// 	if request, err = http.NewRequest(method, rawUrl, bytes.NewBuffer(rawBody)); err != nil {
-// 		return nil, err
-// 	}
-// 	request.Header.Set("Content-Type", "application/json;charset=UTF-8")
-//
-// 	return
-// }
-//
-// func executeHTTP(req *http.Request) (rawResp []byte, err error) {
-// 	start := time.Now()
-// 	var resp *http.Response
-// 	if resp, err = HTTPClient.Do(req); err != nil {
-// 		return nil, err
-// 	}
-// 	defer func() {
-// 		_, _ = io.Copy(ioutil.Discard, resp.Body)
-// 		_ = resp.Body.Close()
-// 	}()
-//
-// 	rawResp, err = ioutil.ReadAll(resp.Body)
-// 	debugLog(fmt.Sprintf("<-- %s %s %d %s\n%s\n", req.Method, req.URL.String(), resp.StatusCode, time.Since(start), rawResp))
-// 	if err != nil {
-// 		return nil, err
-// 	}
-//
-// 	var reply = new(struct {
-// 		Code int    `json:"code"`
-// 		Msg  string `json:"msg"`
-// 	})
-// 	if err = json.Unmarshal(rawResp, reply); err != nil {
-// 		return nil, fmt.Errorf("unknown response: %w\nraw response: %s", err, rawResp)
-// 	}
-// 	if reply.Code != 0 {
-// 		return nil, fmt.Errorf("%s (code: %d)", reply.Msg, reply.Code)
-// 	}
-//
-// 	return
-// }
-//
-// var debugFlag = false
-//
-// func SetDebug(debug bool) {
-// 	debugFlag = debug
-// }
-//
-// func debugLog(msg string) {
-// 	if !debugFlag {
-// 		return
-// 	}
-// 	log.Println("[DEBUG-FeiShu-Bot-API] " + msg)
-// }
